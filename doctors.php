fa6fb40b3af4acc7c3ef0034458b8dd72aa25ac4
SVinisha <145470012+SVinisha@users.noreply.github.com>
2023-12-19 21:12:53 +0530

    Add files via upload

---
+++ doctors.php
@@ -0,0 +1,155 @@
+ <?php 
+ include "includes/header.php";
+
+ ?> 
+ <html>
+<head>
+ <title>Clinical Services | Doctor Page</title>
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+<link href="css/responsive.css" rel="stylesheet">
+<link href="vendor/jquery-ui/jquery-ui.css" rel="stylesheet">
+<link href="vendor/time-picker/jquery.timepicker.css" rel="stylesheet">
+
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<style>
+
+</style>
+</head>
+
+<div class="container">
+
+<div class="row">
+<br><br>
+<div class="col-sm-4">
+    <img src="images/d1.jpg" style="width:400px;height:200px;">
+    <h4 style="font-weight:bold;color:blue;">Epidemiologist</h4>
+    <p>Epidemiologists look for potential diseases that could appear, causing a lot of problems. 
+    They search for vaccinations for current terminal diseases such as HIV/AIDS and cancer.</p>
+</div>
+
+<div class="col-sm-4">
+
+<img src="images/d2.jpg" style="width:400px;height:200px;">
+    <h4 style="font-weight:bold;color:blue;">Podiatrist</h4>
+    <p>A podiatrist cares for issues afflicting the feet or ankles. They are often called foot doctors, treating afflictions such as calluses, nail disorders, athlete's foot or other foot infections or injuries.</p>
+
+</div>
+
+<div class="col-sm-4">
+    <img src="images/d3.jpg" style="width:400px;height:200px;">
+    <h4 style="font-weight:bold;color:blue;">Audiologist</h4>
+    <p>
+    An audiologist specializes in ear-related issues and fit all ages with hearing aids to improve hearing and address hearing loss. Generally, audiologists work in audiology clinics, hospitals, physicians' offices and sometimes in schools.
+    </p>
+</div>
+
+</div>
+<div class="row">
+<br><br>
+<div class="col-sm-4">
+    <img src="images/d4.jpg" style="width:400px;height:200px;">
+    <h4 style="font-weight:bold;color:blue;">General practitioner</h4>
+    <p>
+    A general practitioner or family physician cares for the whole family from children to the elderly, giving basic care. They do routine check-ups and screening tests, attending to ongoing medical conditions. Usually, they are the first to recognize major health problems, referring patients to a specialist.
+    </p>
+</div>
+
+<div class="col-sm-4">
+
+<img src="images/d5.jpg" style="width:400px;height:200px;">
+    <h4 style="font-weight:bold;color:blue;">Pediatrician</h4>
+    <p>
+    A pediatrician cares for children from birth to adolescence, treating them for a variety of health issues from a common cold to more severe conditions. Their consolation rooms are usually bright and cheerful, appealing to children.
+    </p>
+
+</div>
+
+<div class="col-sm-4">
+    <img src="images/d6.jpg" style="width:400px;height:200px;">
+    <h4 style="font-weight:bold;color:blue;">Obstetrics and gynecology physician</h4>
+    <p>
+    Obstetrics and gynecology physicians specialize in medical care related to pregnancy and childbirth. They help to prevent diseases related to the reproductive system.
+    </p>
+</div>
+
+</div>
+
+<div class="row">
+<br><br>
+<div class="col-sm-4">
+    <img src="images/d7.jpg" style="width:400px;height:200px;">
+    <h4 style="font-weight:bold;color:blue;">Otolaryngologist/ENT specialist</h4>
+    <p>
+    An otolaryngologist is commonly known as an "ENT specialist" who concentrates on issues related to the ear, nose and throat. They also care for diseases related to the neck and heat, doing reconstructive and plastic surgery on the neck and head.
+    </p>
+</div>
+
+<div class="col-sm-4">
+
+<img src="images/d8.jpg" style="width:400px;height:200px;">
+    <h4 style="font-weight:bold;color:blue;">Oncologist</h4>
+    <p>
+    Oncologists care for cancer patients. They focus on the treatment and prevention of cancer, administering chemotherapy and working along with radiation oncologists and surgeons to help patients suffering from cancer. Oncologists spend time with cancer survivors through follow-up sessions.
+    </p>
+
+</div>
+
+<div class="col-sm-4">
+    <img src="images/d9.jpg" style="width:400px;height:200px;">
+    <h4 >Dentist</h4>
+    <p>
+    Dentists are doctors of the mouth. They examine teeth and gum health, checking for different issues such as bleeding gums and cavities.
+    </p>
+</div>
+
+</div>
+
+<!-- second row -->
+
+<div class="row">
+
+<div class="col-sm-4">
+</div>
+
+<div class="col-sm-4">
+</div>
+
+<div class="col-sm-4">
+</div>
+
+</div>
+
+<!--  thired row -->
+
+
+<div class="row">
+
+<div class="col-sm-4">
+
+</div>
+
+<div class="col-sm-4">
+
+</div>
+
+<div class="col-sm-4">
+    
+</div>
+
+</div>
+
+
+</div>
+
+<?php include "includes/footer.php" ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,99 @@
+<html>
+<title>Clinical Services | Home</title>
+
+
+<!-- Stylesheets -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/revolution-slider.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<!-- Responsive -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<link href="css/bootstrap-margin-padding.css" rel="stylesheet">
+<link href="css/responsive.css" rel="stylesheet">
+<link href="vendor/jquery-ui/jquery-ui.css" rel="stylesheet">
+<link href="vendor/time-picker/jquery.timepicker.css" rel="stylesheet">
+
+
+
+ <?php include "includes/header.php" ?>
+
+   <hr>
+  
+    <!--Featured Three Column-->
+    <section class="featured-three-column">
+    	<div class="auto-container">
+            <div class="row clearfix">
+            	  
+                <!--Column-->
+                <div class="col-md-4   fadeInLeft" data-wow-delay="200ms" data-wow-duration="1500ms"><br><br>
+                    <article class="inner-box">
+                        <figure class="image">
+                            <a href="#"><img src="images/resource/featured-image-1.jpg" alt=""></a>
+                            <div class="overlay"><span class="left-curve"></span><span class="right-curve"></span><a href="#" class="overlay-link"></a></div>
+                        </figure>
+                        <div class="content">
+                            <h3>Doctors</h3>
+                            <div class="text">
+                                <p>For Doctors Only</p>
+                            </div>
+                            <div class="link">
+                                <a href="doctor/index.php" class="read-more normal-btn theme-btn"></span>LOGIN</a>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+                
+                <!--Column-->
+                <div class="col-md-4   fadeInUp" data-wow-delay="700ms" data-wow-duration="1500ms"><br><br>
+                    <article class="inner-box">
+                        <figure class="image">
+                            <a href="#"><img src="images/resource/featured-image-2.jpg" alt=""></a>
+                            <div class="overlay"><span class="left-curve"></span>
+                                <span class="right-curve"></span><a href="#" class="overlay-link"></a></div>
+                        </figure>
+                        <div class="content">
+                            <h3>Admin</h3>
+                            <div class="text">
+                                <p>For Admin Only</p>
+                            </div>
+                            <div class="link"><a href="admin/index.php" class="read-more normal-btn theme-btn"> LOGIN</a></div>
+                        </div>
+                    </article>
+                </div>
+                
+                <!--Column-->
+                <div class="col-md-4   fadeInRight" data-wow-delay="200ms" data-wow-duration="1500ms"><br><br>
+                    <article class="inner-box">
+                        <figure class="image">
+                            <a href="#"><img src="images/resource/featured-image-3.jpg" alt=""></a>
+                            <div class="overlay"></span><span class="right-curve"></span><a href="#" class="overlay-link"></a></div>
+                        </figure>
+                        <div class="content">
+                            <h3>Users</h3>
+                            <div class="text">
+                                <p>For Users Only</p>
+                            </div>
+                            <div class="link"><a href="user/index.php" class="read-more normal-btn theme-btn"></span>LOGIN</a></div>
+                        </div>
+                    </article>
+                </div>
+                
+                
+            </div>
+        </div>
+    </section>
+    
+    <h1><hr></h1>
+
+  
+    
+
+
+<script src=""></script>
+<script src="js/respond.js"></script>
+<script src="js/jquery.js"></script> 
+<script src="js/bootstrap.min.js"></script>
+
+
+<?php include "includes/footer.php" ?>
